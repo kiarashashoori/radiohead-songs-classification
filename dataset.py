1df900ae1583be35ba66e6253108a99a3666daf7
kiarashashoori <ashoorikiarash8585@gmail.com>
2026-06-09 19:03:42 +0330

    create my own dataset

---
+++ dataset.py
@@ -0,0 +1,38 @@
+from parameters import Parameters as p
+from pydub import AudioSegment
+import os
+
+
+class ExtractDataset():
+    def __init__(self):
+        self.music_path = p.music_path
+        self.dataset_path = p.dataset_path
+
+        self.each_clip_dur = 10000
+        self.between_clip_dur = 10000
+
+        self.music_classes = os.listdir(self.music_path)
+    
+    def create_music_folder(self):
+        self.music_classes = os.listdir(self.music_path)
+        for music in self.music_classes:
+            if not music.endswith(p.music_formats):
+                self.music_classes.remove(music)
+                continue
+            os.mkdir(p.dataset_path+music[:len(music)-4])
+    
+    def clip_audios(self):
+
+        for file in self.music_classes:
+            audio = AudioSegment.from_file(self.music_path+file)
+            song_duration = len(audio)
+            for i in range(0,int(song_duration//(self.between_clip_dur+self.each_clip_dur))):
+                start_time = i*(self.between_clip_dur+self.each_clip_dur)
+                end_time = start_time+self.each_clip_dur
+                clipped = audio[start_time:end_time]
+                output_path = self.dataset_path+file[:len(file)-4]+'/'
+                clipped.export(output_path+str(i), format='mp3')
+
+dataset = ExtractDataset()
+dataset.create_music_folder()
+dataset.clip_audios()
---
+++ parameters.py
@@ -0,0 +1,5 @@
+class Parameters():
+
+    music_path = 'music/'
+    dataset_path = 'dataset/'
+    music_formats = ('mp3','m4a')
