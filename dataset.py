936e1af772dbe43fbb8bbeb9678e3e11b8258c8b
kiarashashoori <ashoorikiarash8585@gmail.com>
2026-06-10 00:06:38 +0330

    add validation dataset

--- dataset.py
+++ dataset.py
@@ -18,17 +18,17 @@ class ExtractDataset():
 
         self.music_classes = os.listdir(self.music_path)
     
-    def create_music_folder(self):
+    def create_classes_folder(self,mode = 'train'):
         self.music_classes = os.listdir(self.music_path)
         for music in self.music_classes:
             if not music.endswith(p.music_formats):
                 self.music_classes.remove(music)
                 logging.info(f"unkown file '{music}'")
                 continue
-            os.mkdir(p.dataset_path+music[:len(music)-4])
-            logging.info(f"directory for '{music[:len(music)-4]}' class created")
+            os.mkdir(p.dataset_path+mode+'/'+music[:len(music)-4])
+            logging.info(f"directory for '{music[:len(music)-4]}' class created ---- {mode}")
     
-    def clip_audios(self):
+    def clip_train_audios(self):
 
         for file in self.music_classes:
             audio = AudioSegment.from_file(self.music_path+file)
@@ -37,10 +37,24 @@ class ExtractDataset():
                 start_time = i*(self.between_clip_dur+self.each_clip_dur)
                 end_time = start_time+self.each_clip_dur
                 clipped = audio[start_time:end_time]
-                output_path = self.dataset_path+file[:len(file)-4]+'/'
-                clipped.export(output_path+str(i), format='mp3')
-            logging.info(f"{i} labels created for '{file[:len(file)-4]}' class")
+                output_path = self.dataset_path+'train/'+file[:len(file)-4]+'/'
+                clipped.export(output_path+str(i)+".mp3", format='mp3')
+            logging.info(f"{i} labels created for '{file[:len(file)-4]}' class ------ train")
+
+    def clip_val_audios(self):
+        for file in self.music_classes:
+            audio = AudioSegment.from_file(self.music_path+file)
+            song_duration = len(audio)
+            for i in range(0,int(song_duration//(self.between_clip_dur+self.each_clip_dur))):
+                start_time = i*(self.between_clip_dur+self.each_clip_dur)+self.each_clip_dur
+                end_time = start_time+self.between_clip_dur
+                clipped = audio[start_time:end_time]
+                output_path = self.dataset_path+'val/'+file[:len(file)-4]+'/'
+                clipped.export(output_path+str(i)+".mp3", format='mp3')
+            logging.info(f"{i} labels created for '{file[:len(file)-4]}' class ----- val")
 
 dataset = ExtractDataset()
-# dataset.create_music_folder()
-dataset.clip_audios()
+dataset.create_classes_folder(mode='train')
+dataset.create_classes_folder(mode='val')
+dataset.clip_train_audios()
+dataset.clip_val_audios()
